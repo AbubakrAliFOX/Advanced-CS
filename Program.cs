c0ad3886a70a7bc229b565beae4528291f2dc4d4
Abubakr <thefox5000@gmail.com>
2024-11-20 08:40:27 +0300

    Nullables

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? Age = null;
+
+            string PersonAge = Age?.ToString() ?? "Hi";
+
+            Console.WriteLine("Age is " + PersonAge);
+        }
+    }
+}
